e255f9f768dc6084105be33a3e213b1e6d5d983d
Naveen Kumar Reddy Chinthakunta <89237138+Naveenreddy0018@users.noreply.github.com>
2023-07-18 12:10:04 +0530

    Add files via upload

---
+++ HashPractice.java
@@ -0,0 +1,41 @@
+import java.util.Map;
+import java.util.HashMap;
+
+public class HashPractice {
+    // public static int customHashFunction(String input){
+    //     int hashCode=0;
+    //     for(int i=0;i<input.length();i++){
+    //         hashCode += input.charAt(i);
+    //     }
+    //     return hashCode;
+
+    // }
+    // public static void main(String[] args){
+    //     String name ="Gousiya";
+    //     int hashCode = customHashFunction(name);
+    //     System.out.println(name);
+    //     System.out.println(hashCode);
+
+    // }
+    // public static void main(String[] args) {
+    //     Map<Integer, String> dataMap = new HashMap<>();
+
+    //     String name = "Naveen";
+    //     int hashCode = name.hashCode();
+
+    //     dataMap.put(hashCode, name);
+    //     String outputName = dataMap.get(hashCode);
+    //     System.out.println("Name : " +outputName);
+    // }
+    public static void main(String[] args) {
+        Map<Integer, String> dataMap = new HashMap<>();
+
+        String name = "Pushpanjali";
+        int hashCode = name.hashCode();
+        System.out.println(hashCode);
+        // Key => hashCode
+        dataMap.put(hashCode, name);
+        String outputName = dataMap.get(hashCode);   
+        System.out.println("Name: " + outputName);
+    }
+}
